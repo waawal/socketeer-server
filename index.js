871631e2c49b6be31d1187941a2801c81d790440
Daniel Waardal <daniel@massforstroel.se>
2013-07-06 16:35:11 +0200

    fixing imports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var server, port;
-require('coffeescript')
-server = require('app');
+require('coffee-script')
+server = require('./app');
 port = server.port;
 server.listen(port, function() {
   return console.log("Listening on " + port + "\nPress CTRL-C to stop server.");
